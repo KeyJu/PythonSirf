207b3baa7da7996ec9bb73c54bc830a8ad49e384
Ju <lipengju@icegps.com>
2016-06-28 17:09:59 +0800

    python解析sirf数据

--- PythonSirf/PythonSirf.py
+++ PythonSirf/PythonSirf.py
@@ -1,25 +1,28 @@
 #coding:utf-8
-  
+ 
 filename = ['sirf 001.gps','sirf 002.gps']
+name = ['Satellite ID','GPS Time','Pseudorange','Carrier Phase','C/N']
 i = 1 
 for fn in filename:
     fr = open(fn,'r')
     fw = open('sirf%d.txt' % i,  'w')  
-    buffer = []  
-
+    buffer = []
+    print(name[0].rjust(15)+name[1].rjust(18)+name[2].rjust(20)+name[3].rjust(20)+name[4].rjust(10))
+    fw.write(name[0].rjust(15)+name[1].rjust(18)+name[2].rjust(20)+name[3].rjust(20)+name[4].rjust(10))
+    fw.write("\n")        
     for line in open(fn):  
-        line = fr.readline() 
+        line = fr.readline
         buffer = line.split(',')
         if buffer[0] == '28':
             need = [] 
-            need.append( buffer[0])
-            need.append( buffer[1])
-            need.append( buffer[2])
+            need.append(buffer[3])
+            need.append( ("%0.3f" % float(buffer[4])))
+            need.append( ("%0.3f" % float(buffer[5])))
             need.append( ("%0.3f" % float(buffer[7])))
-            need.append( buffer[10])        
-            print (need)
-            fw.write(str(need))
-            fw.write("\n")   
+            need.append( buffer[10])                      
+            print(need[0].rjust(15)+need[1].rjust(18)+need[2].rjust(20)+need[3].rjust(20)+need[4].rjust(10))
+            fw.write(need[0].rjust(15)+need[1].rjust(18)+need[2].rjust(20)+need[3].rjust(20)+need[4].rjust(10))
+            fw.write("\n")  
     i = i+1
     fr.close()
     fw.close()    
